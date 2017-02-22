409af5819d9eb0e0d096aabccc8af27161f78f7d
Anna Pigalova <annpigalova@gmail.com>
2017-02-23 00:23:36 +0300

    Fixed Or statement

--- src/Task6.java
+++ src/Task6.java
@@ -42,7 +42,7 @@ public class Task6 {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 
-				if (i == 0 | i == n - 1 | j == 0 | j == n - 1) {
+				if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
 					matrix[i][j] = 1;
 				} else {
 					matrix[i][j] = 0;
